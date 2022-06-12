639b3b639d409b0151757a9192dde79abe8a699d
Ryan Villanueva <54481319+Wasabi199@users.noreply.github.com>
2022-06-12 15:55:55 +0800

    Initial Commit

---
+++ commision/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Partials/CSS/index.css">
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <div class="logo">
+            <img src="Partials/Images/logo.png" class="logo">
+        </div>
+    </div>
+</div>
+
+</body>
+
+
+</html>
