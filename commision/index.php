18b8fd0082c4e040d2bbc0c23e7daa3b76a94ae5
Revenger15 <55306826+Revenger15@users.noreply.github.com>
2022-06-12 19:37:11 +0800

    Login UI
    Login design

--- commision/index.php
+++ commision/index.php
@@ -2,16 +2,71 @@
 <head>
     <link rel="stylesheet" type="text/css" href="Partials/CSS/index.css">
 </head>
+
 <body>
-<div class="container">
-    <div class="header">
-        <div class="logo">
-            <img src="Partials/Images/logo.png" class="logo">
-        </div>
+<div class="login-header-1">
+    <p class="text-header">PSU-URDA SCHOLARSHIP RETENTION PREDICTING SYSTEM(RSPS)</p>
+    <div class="login-logo">        
+        <img class="logo" src="Partials/Images/logo.png">
+    </div>
+</div>
+<div class="login-header-2">
+    <div class="link">
+        <a href="">Policy</a> | <a href="">Help</a>
+    </div>
+</div>
+<div class="login-header-3">
+    <div class="header-text1">
+        <h4>Student Service Office</h4>        
+    </div>
+    <div class="header-text2">        
+        <h2>LOGIN TO PSU</h2>
     </div>
 </div>
 
+<div class="line-link">
+    <a href="">http/ahgha/ga/ga/sg/asg/asg/as</a>
+</div>
+<div class="line-link0">
+    <a href="">Enter your username and password to enter the system</a>
+</div>
+
+<div>
+    <center>
+    <form action="">
+        <table class="container">
+           <tr>
+            <td style="text-align: left;">Username:</td>
+            <td style="text-align: center;"><input type="text"></td>
+           </tr>
+           <tr>
+            <td style="text-align: left;">Password:</td>
+            <td style="text-align: center;"><input type="password"></td>
+           </tr>
+           <tr>
+            <td align="right" width="25%">&nbsp;</td>
+            <td class="submit">
+              <input class="btn" type="submit" value="Login">&nbsp;
+              <input class="btn" type="Reset" value="Reset"></td>
+          </tr>
+        </table>
+    </form>
+    </center>
+</div>
 </body>
 
-
+<footer>
+    <div class="link2">
+        <a href="">Policy</a> | <a href="">Help</a>
+    </div>
+    <div class="line-link-footer">
+        <a href="">http/ahgha/ga/ga/sg/asg/asg/as</a>
+    </div>
+    <div class="footer-text">
+        <p>All right chcuhcuchcuchucuhcuhc</p>
+        <p>8th akhjfgasdg fas</p>
+        <p>Pasig ahjfgvihasod</p>
+        <p>Teleao huagoi ugasfsadfoi</p>
+    </div>
+</footer>
 </html>
